efe799f08443f287cb27101092a36408c0ddd053
kirill <shkolenko02-03-2004@mail.ru>
2023-12-02 14:23:06 +0300

    Updated docker compose and ml

--- routing/v1/ml.py
+++ routing/v1/ml.py
@@ -27,26 +27,3 @@ async def post(req: MlRequest):
         """- Хз что тут написать :)""",
     )
 
-
-@router.get(
-    "/",
-    response_model=MlResponse,
-)
-async def get(desc: str):
-    if desc == "":
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="There is not description")
-
-    return MlResponse(
-        economy=
-        """- Много расти\n- Сильно расти\n- Широко расти""",
-        business_model=
-        """- продавать за дорого\n- покупать за дешево""",
-        competitors=
-        """- ООО Рога и копыта\n- ООО Тест Сибирь""",
-        development_plan=
-        """- много покупать\n- много продовать""",
-        team=
-        """- дизайнер\n- бог""",
-        value_proposition=
-        """- Хз что тут написать :)""",
-    )
