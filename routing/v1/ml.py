a0d8997619b18d52850eca62b7ba022a4d575f75
kirill <shkolenko02-03-2004@mail.ru>
2023-12-02 19:37:44 +0300

    Refactored code

--- routing/v1/ml.py
+++ routing/v1/ml.py
@@ -6,24 +6,16 @@ router = APIRouter(prefix="/api/v1/ml", tags=["ml"])
 
 @router.post(
     "/",
-    response_model=MlResponse,
 )
 async def post(req: MlRequest):
     if req.description == "":
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="There is not description")
 
-    return MlResponse(
-        economy=
-        """- Много расти\n- Сильно расти\n- Широко расти""",
-        business_model=
-        """- продавать за дорого\n- покупать за дешево""",
-        competitors=
-        """- ООО Рога и копыта\n- ООО Тест Сибирь""",
-        development_plan=
-        """- много покупать\n- много продовать""",
-        team=
-        """- дизайнер\n- бог""",
-        value_proposition=
-        """- Хз что тут написать :)""",
-    )
-
+    return {
+        "Экономика": "- Много расти\n- Сильно расти\n- Широко расти",
+        "Бизнес модель": "- продавать за дорого\n- покупать за дешево",
+        "Конкуренты": "- ООО Рога и копыта\n- ООО Тест Сибирь",
+        "План развития": "- много покупать\n- много продовать",
+        "команда": "- дизайнер\n- бог",
+        "ценностное предложение": "- Хз что тут написать :)",
+    }
