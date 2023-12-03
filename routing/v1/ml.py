40832077eea6d6c1650c2939555010e075ea0108
kirill <shkolenko02-03-2004@mail.ru>
2023-12-03 09:23:18 +0300

    Added the ssl

--- routing/v1/ml.py
+++ routing/v1/ml.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, HTTPException, status
 from schemas.ml import MlResponse, MlRequest
+from langchain.chat_models.gigachat import GigaChat
 
 router = APIRouter(prefix="/api/v1/ml", tags=["ml"])
 
@@ -10,6 +11,8 @@ router = APIRouter(prefix="/api/v1/ml", tags=["ml"])
 async def post(req: MlRequest):
     if req.description == "":
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="There is not description")
+
+    # chat = GigaChat(credentials="", verify_ssl_certs=False)
 
     return {
         "Экономика": "- Много расти\n- Сильно расти\n- Широко расти",
